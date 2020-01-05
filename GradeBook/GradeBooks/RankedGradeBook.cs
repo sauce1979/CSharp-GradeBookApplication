ec6dd8168cfe7ac97aae0c438feb0cbf587a7a71
sauce1979 <59530608+sauce1979@users.noreply.github.com>
2020-01-05 13:40:49 +0100

    made changes for module 1 and 2

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name): base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            List<double> ListOfGrades = new List<double>();
+            int Threshold = (int)(Math.Ceiling(Students.Count * 0.2));
+            if(Students.Count < 5)
+            throw new InvalidOperationException("Ranked - grading requires a minimum of 5 students to work");
+            foreach(var student in Students)
+            {
+                ListOfGrades.Add(student.AverageGrade);
+                
+            }
+            ListOfGrades.Sort();
+            ListOfGrades.Reverse();
+            if(ListOfGrades[Threshold - 1] <= averageGrade)
+                return 'A';
+            else if(ListOfGrades[Threshold * 2 - 1] <= averageGrade)
+                return 'B';
+            else if (ListOfGrades[Threshold * 3 - 1] <= averageGrade)
+                return 'C';
+            else if (ListOfGrades[Threshold * 4 - 1] <= averageGrade)
+                return 'D';
+            else
+                return 'F';
+        }
+    }
+
+}
